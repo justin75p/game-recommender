a1b50e94ea4dbe469271caf4099118a80d12f634
Justin Phan <justin75p@gmail.com>
2026-03-31 20:58:47 -0700

    function skeletons for database.py

---
+++ src/database.py
@@ -0,0 +1,17 @@
+import psycopg2
+
+class Database():
+    def __init__(self):
+        self.conn = None
+
+    def connect(self):
+        pass
+
+    def close(self):
+        pass
+
+    def save_games(self, games):
+        pass
+
+    def get_all_games(self):
+        pass
