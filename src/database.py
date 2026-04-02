7e9e23d21231b447e2568640ddf3b2f96b20a90f
Justin Phan <justin75p@gmail.com>
2026-04-01 22:00:57 -0700

    implement function to create database table

--- src/database.py
+++ src/database.py
@@ -16,6 +16,26 @@ class Database():
             password=os.getenv("DB_PASSWORD"), 
             port="5432"
         )
+    def create_table(self):
+        if not self.conn:
+            print("No connection found. Call connect() first.")
+            return
+        
+        cursor = self.conn.cursor()
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS games (\
+                id INTEGER PRIMARY KEY,
+                name TEXT NOT NULL,
+                rating REAL,
+                playtime INTEGER,
+                metacritic INTEGER,
+                genres TEXT,
+                tags TEXT
+            )
+        """)
+
+        self.conn.commit()
+        cursor.close()
 
     def close(self):
         if self.conn:
