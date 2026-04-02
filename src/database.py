f908a3a3642b15b43856cbf750b417ab35655c96
Justin Phan <justin75p@gmail.com>
2026-04-01 17:42:18 -0700

    implement connect and close methods for database

--- src/database.py
+++ src/database.py
@@ -1,14 +1,25 @@
 import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Database():
     def __init__(self):
         self.conn = None
 
     def connect(self):
-        pass
+        self.conn = psycopg2.connect(
+            host="localhost",
+            dbname="game_recommender",
+            user="postgres",
+            password=os.getenv("DB_PASSWORD"), 
+            port="5432"
+        )
 
     def close(self):
-        pass
+        if self.conn:
+            self.conn.close()
 
     def save_games(self, games):
         pass
