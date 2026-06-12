14f22b6436c57c3c18beb4637e0538b7df50dbab
Justin Phan <justin75p@gmail.com>
2026-06-12 11:44:33 -0700

    differentiated potential duplicate genre/tags, i.e. RPG showing up as a tag AND genre changed from most common 25 tags to most common 50 tags

--- src/features.py
+++ src/features.py
@@ -19,24 +19,24 @@ def build_feature_matrix(games):
     games_df['genres'] = games_df['genres'].str.split(',').fillna('').apply(list)
     games_df['tags'] = games_df['tags'].str.split(',').fillna('').apply(list)
 
-    # Filter to top 25 most common tags
+    # Filter to top 50 most common tags
     all_tags = games_df['tags'].explode()
-    top_25_tags = all_tags.value_counts().head(25).index
+    top_50_tags = all_tags.value_counts().head(50).index
 
     filtered_tags = []
     for tag_list in games_df['tags']:
-        filtered = [tag for tag in tag_list if tag in top_25_tags]
+        filtered = [tag for tag in tag_list if tag in top_50_tags]
         filtered_tags.append(filtered)
 
     games_df['tags'] = filtered_tags
 
     # One-hot encode genres column
     genres_dummies = games_df['genres'].explode()
-    genres_dummies = pd.get_dummies(genres_dummies).groupby(level=0).max().astype(int)
+    genres_dummies = pd.get_dummies(genres_dummies).groupby(level=0).max().astype(int).add_prefix('genre_')
 
     # One-hot encode tags column
     tags_dummies = games_df['tags'].explode()
-    tags_dummies = pd.get_dummies(tags_dummies).groupby(level=0).max().astype(int)
+    tags_dummies = pd.get_dummies(tags_dummies).groupby(level=0).max().astype(int).add_prefix('tag_')
 
     # Normalize the rating and playtime columns
     scaler = MinMaxScaler()
